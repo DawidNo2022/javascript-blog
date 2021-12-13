8e2fe64549fcf6f579977513e6428991f3da699b
Dawid Skarwecki <dawidskarwecki@gmail.com>
2021-12-13 22:24:01 +0100

    add js code for finally version

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log(event +'Link was clicked!');
-
-  /* remove class 'active' from all article links  */
+  console.log(event ,'Link was clicked!');
+  event.preventDefault();
+  const clickedElement=this;
+  /*[DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [Done]add class 'active' to the clicked link */
+  console.log('clicked element',clickedElement);
+  clickedElement.classList.add('active');
+  /*[Done] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
 for(let activeArticle of activeArticles){
@@ -21,9 +23,13 @@ for(let activeArticle of activeArticles){
 
   /* get 'href' attribute from the clicked link */
 
+let link=clickedElement.getAttribute('href');
+console.log(link,'Pobrany href');
   /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle=document.querySelector(link);
+    console.log(targetArticle,'Wybrany artykul to');
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
